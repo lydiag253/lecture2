7b2f96cabd35e2bcb7a6af672c12ed63a6d5af74
Nicole Logan <nicolelogan@MacBook-Pro-2.local>
2019-07-06 08:48:05 -0400

    added template

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world!"
-
-@app.route("/<string:name>")
-def hello():
-    return "Hello, {}!".format(name)
+    return render_template("index.html")
